cef4e3685ed4ca6d8d8013c5f0fca18aff81f37c
rodolfolsouza <63168467+rodolfolsouza@users.noreply.github.com>
2023-05-19 17:31:14 -0300

    Update 2_Logs.py

--- pages/2_Logs.py
+++ pages/2_Logs.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import streamlit as st
 from loguru import logger
 
+with open('style.css') as ww:
+    st.markdown(f'<style>{ww.read()}</style>', unsafe_allow_html=True)
+
 logname = "log_geral.log"
 
 logger.add(logname)
